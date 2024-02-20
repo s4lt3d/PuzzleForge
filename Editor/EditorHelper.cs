a52e12da37f91eac8e45de6228d88cf64529cbcf
Walter Gordy <walter.gordy@gmail.com>
2024-02-20 14:21:50 -0700

    Draws better curves

--- Editor/EditorHelper.cs
+++ Editor/EditorHelper.cs
@@ -1,20 +1,18 @@
-﻿
-
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 namespace PuzzleForge
 {
     public static class EditorHelper
     {
-        
-        public static void DrawCurve(Vector3 pos1, Vector3 pos2, Color color, float curveRatio, int width=1)
+        public static void DrawCurve(Vector3 pos1, Vector3 pos2, Color color, float curveRatio, int width = 1)
         {
-            Vector3 start = pos1;
+            var start = pos1;
+            var end = pos2;
 
-            Vector3 end = pos2;
+            if (start.y < end.y) (start, end) = (end, start);
 
-            float offset = Mathf.Abs(start.y - end.y) * curveRatio;
+            var offset = Mathf.Abs(start.y - end.y) * curveRatio;
 
             Handles.DrawBezier(
                 start,
