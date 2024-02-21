848e28ab4c72d3e2fc53e84279af2bc685ef297c
Walter Gordy <walter.gordy@gmail.com>
2024-02-21 12:50:08 -0700

    Updated texture format and enhanced signal handling
    PFSignalBase was refactored for better readability. This includes improvements in handling trigger interactions, with additional checks and modified logic within the SendSignal method to enhance signal processing.

--- Runtime/PFSignalBase.cs
+++ Runtime/PFSignalBase.cs
@@ -24,95 +24,105 @@ namespace PuzzleForge
     }
 
     public class PFSignalBase : PFBase
-{
-    public TriggerType triggerType;
-    public TriggerMode triggerMode;
-    public TriggerInteractions triggerInteractions;
-    [Range(0, 10)]
-    public int activationTagCount = 1;
-    [Range(0, 10)]
-    public int deactivateTagCount;
-    public List<string> interactionTags = new() { "Player" };
-    [HideInInspector]
-    public List<PFEventReactor> activationReactors;
-    [HideInInspector]
-    public List<PFEventReactor> deactivationReactors;
-    public bool mouseClickDebug;
-    private bool hasFired;
-    private bool mouseState;
-    private bool state;
-    protected List<string> tagsActive = new();
-
-    private void OnMouseDown()
     {
-        if (mouseClickDebug)
+        public TriggerType triggerType;
+        public TriggerMode triggerMode;
+        public TriggerInteractions triggerInteractions;
+
+        [Range(0, 10)]
+        public int activationTagCount = 1;
+
+        [Range(0, 10)]
+        public int deactivateTagCount;
+
+        public List<string> interactionTags = new() { "Player" };
+
+        [HideInInspector]
+        public List<PFEventReactor> activationReactors;
+
+        [HideInInspector]
+        public List<PFEventReactor> deactivationReactors;
+
+        public bool mouseClickDebug;
+        private bool hasFired;
+        private bool mouseState;
+        private bool state;
+        protected List<string> tagsActive = new();
+
+        private void OnMouseDown()
         {
-            mouseState = !mouseState;
-            if (mouseState)
+            if (mouseClickDebug)
             {
-                Debug.Log("Debug Activate Event");
-                SendSignal(null, true);
-            }
-            else
-            {
-                DebugActivate();
+                mouseState = !mouseState;
+                if (mouseState)
+                {
+                    Debug.Log("Debug Activate Event");
+                    SendSignal(null, true);
+                }
+                else
+                {
+                    DebugActivate();
+                }
             }
         }
-    }
 
-    protected void SendSignal(Component component, bool isActive)
-    {
-        // Included objectTags in the check
-        if (!interactionTags.Contains(component.tag) && !objectTags.Contains(component.tag)) 
-            return;
-
-        if (isActive)
-            HandleTriggerInteractions(component, activationTagCount, TriggerInteractions.DeactivationOnly);
-        else
-            HandleTriggerInteractions(component, deactivateTagCount, TriggerInteractions.ActivationOnly);
-
-        if (triggerType == TriggerType.Latching && hasFired) return;
-        hasFired = true;
-
-        SetNextState(isActive);
-
-        if (state && triggerInteractions != TriggerInteractions.DeactivationOnly)
-            parentController.DispatchSignal(this, true);
-
-        if (!state && triggerInteractions != TriggerInteractions.ActivationOnly)
-            parentController.DispatchSignal(this, false);
-    }
-
-    protected void HandleTriggerInteractions(Component component, int tagCount, TriggerInteractions interactions)
-    {
-        if (component != null)
+        protected void SendSignal(Component component, bool isActive)
         {
-            tagsActive.Add(component.tag);
-            if (tagsActive.Count < tagCount)
+            var pfBaseComponent = component.GetComponent<PFBase>();
+            var componentTag = component.tag;
+
+            if (!(interactionTags.Contains(componentTag) || pfBaseComponent.objectTags.Contains(componentTag)))
+                return;
+
+            if (isActive)
+                HandleTriggerInteractions(component, activationTagCount, TriggerInteractions.DeactivationOnly);
+            else
+                HandleTriggerInteractions(component, deactivateTagCount, TriggerInteractions.ActivationOnly);
+
+            if (triggerType == TriggerType.Latching && hasFired) return;
+            hasFired = true;
+
+            SetNextState(isActive);
+
+            if (state && triggerInteractions != TriggerInteractions.DeactivationOnly)
+                parentController.DispatchSignal(this, true);
+
+            if (!state && triggerInteractions != TriggerInteractions.ActivationOnly)
+                parentController.DispatchSignal(this, false);
+        }
+
+        protected void HandleTriggerInteractions(Component component, int tagCount, TriggerInteractions interactions)
+        {
+            if (component != null)
+            {
+                tagsActive.Add(component.tag);
+                if (tagsActive.Count < tagCount)
+                    return;
+            }
+
+            if (triggerInteractions == interactions)
                 return;
         }
 
-        if (triggerInteractions == interactions)
-            return;
-    }
+        protected void SetNextState(bool isActive)
+        {
+            state = triggerType == TriggerType.Toggle
+                ? !state
+                : triggerMode == TriggerMode.Normal
+                    ? isActive
+                    : !isActive;
+        }
 
-    protected void SetNextState(bool isActive)
-    {
-        state = triggerType == TriggerType.Toggle 
-            ? !state 
-            : triggerMode == TriggerMode.Normal ? isActive : !isActive;
-    }
+        public void DebugActivate()
+        {
+            Debug.Log("Debug Activate Event");
+            SendSignal(null, true);
+        }
 
-    public void DebugActivate()
-    {
-        Debug.Log("Debug Activate Event");
-        SendSignal(null, true);
+        public void DebugDeactivate()
+        {
+            Debug.Log("Debug Deactivate Event");
+            SendSignal(null, false);
+        }
     }
-
-    public void DebugDeactivate()
-    {
-        Debug.Log("Debug Deactivate Event");
-        SendSignal(null, false);
-    }
-}
 }
